90b339e1fa94d40ca4fb2c4d764846c0e12c5972
Louise Andrade <mandeumemailparacordier@gmail.com>
2020-10-21 18:51:39 -0300

    funções e estilos

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,83 @@
+var tempoInicial = $("#tempo-digitacao").text();
+  //Evento de clique do campo de escrita e contagem de palavras e caracteres.
+  var campo = $(".campo-digitacao");
+
+  $(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores(); 
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+function  atualizaTamanhoFrase  () {
 //Identifica a frase escrita no paragrafo. 
 var frase = $(".frase").text(); 
 //Pega a quantidade de palavras da frase.
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+}
 
-//Evento de clique do campo de escrita e contagem de palavras e caracteres.
-var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
 
-    //O S+ é uma expressão regular que busca espaço vazios.
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
+        //O S+ é uma expressão regular que busca espaço vazios.
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+    
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
 
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-});
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                campo.toggleClass("campo-desativado");
+            }
+        }, 1000);
+    });
+}
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function() {
-    var cronometroID = setInterval(function() {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-            console.log(tempoRestante);
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
         }
-    }, 1000);
-});
+    });
+}
+
+function inserePlacar(){
+    var corpoTabela = $(".placar").find("tbody");
+}
+
+function reiniciaJogo(){
+    $("#botao-reiniciar").click(function() {
+        campo.attr("disabled", false);
+        campo.val(" ");
+        $("#contador-palavras").text("0");
+        $("#contador-caracteres").text("0");
+        $("#tempo-digitacao").text(tempoInicial);
+        inicializaCronometro();
+        campo.removeClass("campo-desativado");
+    });
+}
+
+
+
